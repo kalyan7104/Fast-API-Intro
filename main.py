a225b99f5239d546aaf629fe88f934136ee152c1
kalyan7104 <kalyankumarmunjuluri@gmail.com>
2026-03-27 13:37:09 +0530

    feat: add Products model and GET /Products endpoint

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from models import Products
 
 app = FastAPI()
 
 @app.get("/")
 def greet():
-    return "hello All"
+    return "Welcome All"
+
+products=[
+    Products(1,"Asus Laptop","Laptop",1000,5),
+    Products(2,"Dell Laptop","Laptop",100,1)
+]
+
+@app.get("/Products")
+def get_products():
+    return products
 
